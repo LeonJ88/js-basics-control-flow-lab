c9930f355a8d156d170859560da6043a6ef20174
Javier <JavierRLeon@protonmail.com>
2021-03-17 06:09:24 -0400

    commit tree 551a9963f2da96d66be4c2367bd122e900a9c031

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength){
+  let response
+  if (rideLength > 2500) {
+    response = 'No can do.'
+  } else if (rideLength > 2000) {
+    response = 'I will gladly take your thirty bucks.'
+  } else if(rideLength <= 400) {
+    response = 'This one is on me!'
+  }
+   return response
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  const city = "NYC";
+  const cityDestination = destination === city ? "Ok, sounds good." : "No go.";
+
+  return cityDestination;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response = '';
+  switch (tip) {
+    case 'generous':
+        response = "Thank you so much.";
+        break;
+    case 'not as generous':
+        response = "Thank you.";
+        break;
+    default:
+        response = 'Bye.';
+  }
+  return response
 }
